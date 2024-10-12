342bdf74c7de16fbfe626e4766673097485030a9
WesA22 <wesalbright22@gmail.com>
2024-10-11 21:51:38 -0500

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,20 @@ const_initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
   },
 ];
+
+const profileEditButton = document.querySelector(".profile__edit-btn");
+
+const editModal = document.querySelector("#edit-profile-modal");
+const editModalCloseButton = editModal.querySelector(".modal__close-btn");
+
+function openModal() {
+  editModal.classList.add("modal__opened");
+}
+
+function closeModal() {
+  editModal.classList.remove("modal__opened");
+}
+
+profileEditButton.addEventListener("click", openModal);
+
+editModalCloseButton.addEventListener("click", closeModal);
